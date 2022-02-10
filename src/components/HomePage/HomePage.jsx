2771010d9deef73f032a186b92fd0e30cf3d643b
rudenko-vl <vladrudenko7@gmail.com>
2022-02-10 16:59:12 +0200

    add poster_path

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { getTrendingMovies } from "../../services/ApiService";
-import { List } from "./HomePage.styled";
+import { List, Item, Img, Title } from "./HomePage.styled";
 
 function HomePage() {
     const [moviesList, setMoviesList] = useState([]);
+    const imgUrl = 'https://image.tmdb.org/t/p/w400';
     useEffect(() => {
         getTrendingMovies().then((data) => {
             return setMoviesList(data.results)
@@ -16,14 +17,14 @@ function HomePage() {
             {moviesList && moviesList.map(movie => {
                 return (
                     movie.title && (
-                        <li key={movie.id}>
+                        <Item key={movie.id}>
                             <Link to={`/movies/${movie.id}`} state={'/'}>
-                                <p>{movie.title}</p>
-                                {/* <img src={movie.poster_path} alt={movie.id} /> */}
+                                <Img src={`${imgUrl}${movie.poster_path}`} alt={movie.id} />
+                                <Title>{movie.title}</Title>
                             </Link>
                             
 
-                        </li>
+                        </Item>
                   )  
                 )
             })}
--- src/components/HomePage/HomePage.styled.jsx
+++ src/components/HomePage/HomePage.styled.jsx
@@ -1,5 +1,40 @@
 const { default: styled } = require("styled-components");
 
 export const List = styled.ul`
-margin-top: 30px;
+  display: grid;
+  max-width: calc(100vw - 48px);
+  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+  grid-gap: 16px;
+  margin-top: 30px;
+  padding: 0;
+  list-style: none;
+  margin-left: auto;
+  margin-right: auto;
+`
+
+export const Item = styled.li`
+  width: 100%;
+  height: 520px;
+  border-radius: 2px;
+  box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
+    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12); 
+`
+
+export const Img = styled.img`
+  width: 100%;
+  height: 460px;
+  object-fit: cover;
+  transition: transform 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  :hover {
+     transform: scale(1.03);
+     cursor: zoom-in;
+  }
+`
+export const Title = styled.p`
+font-size: 18px;
+font-weight: 700;
+color: black;
+list-style: none;
+text-align: center;
+margin-top: 20px;
 `
